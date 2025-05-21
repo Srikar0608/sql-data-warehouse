2decd1b3e5279e49e06f4485c3d63503f7213d35
Srikar0608 <srikarchowdary3699@gmail.com>
2025-05-21 15:50:54 +0530

    Create database.sql

---
+++ scripts/database.sql
@@ -0,0 +1,18 @@
+USE master;
+GO
+--create the 'Warehouse' Database
+CREATE DATABASE Warehouse;
+GO
+  
+USE Warehouse;
+GO
+--create schemas
+  
+CREATE SCHEMA Bronze;
+GO
+  
+CREATE SCHEMA Sliver;
+GO
+  
+CREATE SCHEMA Gold;
+GO
